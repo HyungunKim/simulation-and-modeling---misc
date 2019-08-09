126e390648536c0f9957dad59f06ed1e2fbb99c9
HyungunKim <51437350+HyungunKim@users.noreply.github.com>
2019-08-09 19:27:02 +0900

    Update network_packet.py

--- Network_packet_simulation/network_packet.py
+++ Network_packet_simulation/network_packet.py
@@ -1,56 +1,101 @@
-from collections import namedtuple
+import numpy as np
+from events import Event, EventQueue
 
-Request = namedtuple("Request", ["arrived_at", "time_to_process"])
-Response = namedtuple("Response", ["was_dropped", "started_at"])
 
 class Buffer:
     def __init__(self, size):
-        self.size = size
-        # self.finish_time is a queue for
-        # keeping track of finishing time of packets to compare with new packet's arrical time.
-        self.finish_time = [] 
-                              
-    def process(self, request): # process methods returns approprient Resopnse.
-        time = -1
-        dropped = True
-        free = False
-        for i in range(self.size):
-            if len(self.finish_time) == 0: # handling the case where Buffer is intirely free.
-                free = True   
-                dropped = False
-                break
-            if request.arrived_at >= self.finish_time[0]: # In this case Buffer gets free space thus current request doesn't drop.
-                # skipping time to current request's arrival time.
-                # in doing so finished requests are popped from the queue.
-                self.finish_time.pop(0) # As a queue, first in is first out (fifo).
-                dropped = False 
-            else: # Need to chech if Buffer has free space. see below (line 40)
-                break
-        if len(self.finish_time) == 0: # Double checks if Buffer is free.
-            free = True   
-            dropped = False
-            
-        if not dropped: # this case we are sure it's not dropped.
-            if free: # Buffer is free, current request is processed at it's arrival time.
-                time = request.arrived_at
-            else:
-                time = self.finish_time[-1] # current request is processed when the last element in queue is done.
-            self.finish_time.append(time + request.time_to_process) # Append to queue when current request is done.
+        self.size = size # constant size of buffer
+        self.Q = 0 # queue of packets. 
+        self.t = 0 # time tracker
+        self.dropped = 0 # number of dropped packets.
+    def is_empty(self):
+        if self.Q == 0:
+            return True
+        else:
+            return False
+    
+    def available(self):
+        if self.Q < self.size:
+            return True
+        else:
+            return False
         
-        elif len(self.finish_time) < self.size: # Checking if Buffer has free space
-            dropped = False
-            time = self.finish_time[-1]
-            self.finish_time.append(time + request.time_to_process)
+    def time(self, dT=0):
+        self.t += dT
+        return self.t
+    def set_time(self, T):
+        self.t = T
+    
+    def enQ(self):
+        self.Q += 1
         
-        return Response(dropped, time)
+    def deQ(self):
+        self.Q -= 1
+    
+    def drop(self):
+        self.dropped += 1
+        
+    def num_dropped(self):
+        return self.dropped
 
+    def reset(self):
+        self.Q = 0
+        self.dropped = 0
 
+class arrival(Event):
+    def __init__(self, arrival_time, process_time):
+        self.t = arrival_time
+        self.process_time = process_time
+    def action(self, queue, buffer):
+        if buffer.is_empty():
+            buffer.enQ()
+            buffer.set_time(self.t + self.process_time)
+            queue.insert(returned((buffer.time())))
+        elif buffer.available():
+            buffer.enQ()
+            queue.insert(returned(buffer.time(dT = self.process_time)))
+        else:
+            buffer.drop()
 
-def process_requests(requests, buffer):
-    responses = []
-    for request in requests:
-        responses.append(buffer.process(request))
-    return responses
+class returned(Event):
+    def __init__(self, processed_time):
+        self.t = processed_time
+    def action(self, queue, buffer):
+        buffer.deQ()
+        
 
+def tester(buffer_size, rps, num_packets = 100, num_tests = 1000):
+    
+    # we are testing in milliseconds
+    # or milli unit time, as it can be scaled naturally.
+    lam = rps/1000
+    
+    # beta is average time interval between two arrival events. (measured compared to processing speed)
+    beta = 1/lam
+    
+    # just using Rayleigh distribution for processing time of requests. 
+    # Can play around with other distributions such as Maxwell-Boltzmann.
+    arrival_times = np.random.exponential(beta, (num_tests, num_packets))
+    process_times = np.random.rayleigh(1, (num_tests, num_packets)) 
+    
+    buffer = Buffer(buffer_size)
+    dropped = np.zeros(num_tests)
+    
+    for i in range(num_tests):
+        arrival_time = 0
+        Q = EventQueue()
+        for j in range(num_packets):
+            arrival_time += arrival_times[i,j]
+            process_time = process_times[i,j]
+            Q.insert(arrival(arrival_time, process_time))
+        
+        while Q.notEmpty():
+            e = Q.next()
+            e.action(Q, buffer)
+        
+        dropped[i] = buffer.num_dropped()
+        buffer.reset() # restarting buffer for next test.
+        Q.reset() # restarting queue for next test.
+    return dropped
 
     
